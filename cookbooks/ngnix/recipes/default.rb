9a28d32c000ad3b30077ea9b11829e85594e31b0
rgundeti <ramesh.gundeti@outlook.com>
2017-07-07 18:18:41 +0000

    commiting my first default recipe

--- cookbooks/ngnix/recipes/default.rb
+++ cookbooks/ngnix/recipes/default.rb
@@ -1,5 +1,23 @@
-ffffff#
 # Cookbook:: ngnix
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+#
+
+
+package 'ngnix' do
+end
+service 'ngnix' do 
+
+	action [ :enable, :start ]
+end
+
+file '/var/www/html/index.html'
+
+	content '<html>This is a placeholder for the home page.</html>'
+	mode '0755'
+	owner 'user'
+	group 'user'
+end
+
+ 
